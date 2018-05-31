c8294023c808f73e5e7fb7366285322e17bcb303
Abhishek Satpathy <abhisheksatpathy28@gmail.com>
2018-05-31 12:59:35 +1000

    Checkpoint commit with promises

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,9 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 $(document).ready(function() {
   var mainMenu;
-  var temp_data;
 
   document.addEventListener("online", online, false);
   document.addEventListener("offline", offline, false);
@@ -28,65 +28,132 @@ $(document).ready(function() {
     document.addEventListener("deviceready", database, false);
   }
   function database(){
-    var bawurradb = null;
-    var backenddata = null;
-      if (device.platform == 'amazon-fireos' || device.platform == 'windows') {
-        bawurradb = window.sqlitePlugin.openDatabase({
-          name: 'bawurradb',
-          location: 'default',
-          androidDatabaseImplementation: 2
-        });
-      }
-      else if (device.platform == 'ios') {
-        bawurradb = window.sqlitePlugin.openDatabase({
-          name: 'bawurradb.sqlite',
-          iosDatabaseLocation: 'Library'
-        });
-      }
-      $.getJSON("http://bawurralibrary.appspot.com/serveculture").done( function(data){
-        st_data = JSON.stringify(data)
-
-        bawurradb.transaction(function (tx){
-          tx.executeSql('DROP TABLE CULTURE;');
-          tx.executeSql('CREATE TABLE IF NOT EXISTS CULTURE (data);', [], function(rs) {
-          console.log("DATABASE CREATED");
-        }, function(error) {
-          console.log('SELECT SQL statement ERROR: ' + error.message);
-        });
-        tx.executeSql('INSERT INTO CULTURE(data) VALUES (?)', [st_data], function(rs, dat) {
-          console.log("INSERTED");
-        }, function(error) {
-          console.log('SELECT SQL statement ERROR: ' + error.message);
-        });
-        tx.executeSql('SELECT * FROM CULTURE;', [], function(tr, dt) {
-          backenddata = dt.rows.item(0).data;
-          console.log(backenddata);
-
-        }, function(error) {
-          console.log('SELECT SQL statement ERROR: ' + error.message);
-        });
-
-      });
+    var p1 = new Promise(function(resolve, reject){
+      $.getJSON("http://bawurralibrary.appspot.com/serveculture").done(function(data){
+        resolve(data);
+    });
   });
+  p1.then(function(data){
+    changeToDataURL(data);
+  });
+  console.log(p1);
   onDeviceReady();
-
 }
 
-function connectdatabase(){
+function changeToDataURL(data){
+  console.log(data);
+  var dat = data;
+  var promises = [];
+  var k = [];
+  for (var i = 0; i < data.length; i++){
+    p.push(new Promise(function(resolve, reject){
+      var k = i;
+      toDataURL(data[k].media_1, function(d){
+        console.log(data, k, data[k]);
+          data[k].media_2 = d;
+          resolve(d);
+        });
+      }).then((d) => {
 
+      }));
+    }
+  //     p.push(new Promise(function(resolve, reject){
+  //       if (data[i].media_2 != 'No Image'){
+  //         toDataURL(data[i].media_2, function(d){
+  //           var prom = new Promise(function(resolve, reject){
+  //             data[k].media_2 = d;
+  //             setTimeout(resolve(), 1000);
+  //           });
+  //           prom.then(resolve());
+  //         });
+  //       }
+  //       else {
+  //         resolve();
+  //       }
+  //
+  //   }));
+  //
+  //
+  // }
+  Promise.all(promises).then(function() {
+    console.warn(data);
+    // setTimeout(function(){ console.log(data); }, 10000);
+  });
 }
+
+function entryToDatabase(data){
+  console.log(data);
+  var bawurradb = null;
+  var backenddata = null;
+    if (device.platform == 'amazon-fireos' || device.platform == 'windows') {
+      bawurradb = window.sqlitePlugin.openDatabase({
+        name: 'bawurradb',
+        location: 'default',
+        androidDatabaseImplementation: 2
+      });
+    }
+    else if (device.platform == 'ios') {
+      bawurradb = window.sqlitePlugin.openDatabase({
+        name: 'bawurradb.sqlite',
+        iosDatabaseLocation: 'Library'
+      });
+    }
+    st_data = JSON.stringify(data);
+    bawurradb.transaction(function (tx){
+      tx.executeSql('DROP TABLE CULTURE;');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS CULTURE (data);', [], function(rs) {
+      console.log("DATABASE CREATED");
+    }, function(error) {
+      console.log('SELECT SQL statement ERROR: ' + error.message);
+    });
+    tx.executeSql('INSERT INTO CULTURE(data) VALUES (?)', [st_data], function(rs, dat) {
+      console.log("INSERTED");
+    }, function(error) {
+      console.log('SELECT SQL statement ERROR: ' + error.message);
+    });
+    tx.executeSql('SELECT * FROM CULTURE;', [], function(tr, dt) {
+      backenddata = dt.rows.item(0).data;
+      console.log(backenddata);
+
+    }, function(error) {
+      console.log('SELECT SQL statement ERROR: ' + error.message);
+    });
+
+  });
+}
+
+function toDataURL(url, callback){
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      callback(reader.result);
+    }
+    reader.readAsDataURL(xhr.response);
+  };
+  xhr.open('GET', url);
+  xhr.responseType = 'blob';
+  xhr.send();
+}
+
+
 
 
   function offline(){
     document.addEventListener("deviceready", onDeviceReady, false);
   }
 
-  function onDeviceReady() {
+
+
+
+function onDeviceReady() {
 
     $.getJSON("data/mainMenu.json", function(result) {
       mainMenu = result;
     }).done(parsePage);
   }
+
+
 
 
 var parsePage = function() {
@@ -132,7 +199,6 @@ var parsePage = function() {
       $("<bold></bold>").text(itemList[item]).css('text-transform', 'capitalize').
       appendTo($(".menuContainer").children().last().children().last().find($("center")));
     }
-
 
     $(document).foundation();
 
--- www/js/index.js
+++ www/js/index.js
@@ -16,9 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 $(document).ready(function() {
   var mainMenu;
-  var temp_data;
 
   document.addEventListener("online", online, false);
   document.addEventListener("offline", offline, false);
@@ -28,60 +28,147 @@ $(document).ready(function() {
     document.addEventListener("deviceready", database, false);
   }
   function database(){
-    var bawurradb = null;
-    var backenddata = null;
-      if (device.platform == 'amazon-fireos' || device.platform == 'windows') {
-        bawurradb = window.sqlitePlugin.openDatabase({
-          name: 'bawurradb',
-          location: 'default',
-          androidDatabaseImplementation: 2
-        });
-      }
-      else if (device.platform == 'ios') {
-        bawurradb = window.sqlitePlugin.openDatabase({
-          name: 'bawurradb.sqlite',
-          iosDatabaseLocation: 'Library'
-        });
-      }
-      $.getJSON("http://bawurralibrary.appspot.com/serveculture").done( function(data){
-        st_data = JSON.stringify(data);
+    var p1 = new Promise(function(resolve, reject){
+      $.getJSON("http://bawurralibrary.appspot.com/serveculture").done(function(data){
+        resolve(data);
+    });
+  });
+  p1.then(function(data){
+    changeToDataURL(data);
+  });
+  onDeviceReady();
+}
 
-        bawurradb.transaction(function (tx){
-          tx.executeSql('DROP TABLE CULTURE;');
-          tx.executeSql('CREATE TABLE IF NOT EXISTS CULTURE (data);', [], function(rs) {
+function changeToDataURL(data){
+  var promises = [];
+  var k = [];
+  for (var i = 0; i < data.length; i++){
+    promises.push(new Promise(function(resolve, reject){
+      var k = i;
+      if (data[i].media_1 != 'No Image'){
+        toDataURL(data[k].media_1, function(d){
+            data[k].media_1 = d;
+            resolve();
+          });
+      }
+      else{
+        resolve();
+      }
+    }));
+    promises.push(new Promise(function(resolve, reject){
+      var k = i;
+      if (data[i].media_2 != 'No Image'){
+        toDataURL(data[k].media_2, function(d){
+            data[k].media_2 = d;
+            resolve();
+          });
+      }
+      else{
+        resolve();
+      }
+    }));
+    }
+
+  Promise.all(promises).then(function() {
+    var dataArray = [];
+    new Promise(function(resolve, reject){
+      for (var i = 0; i < data.length; i++){
+        var x = i;
+        dataArray.push(JSON.stringify(data[x]));
+      }
+      console.log("Here"+dataArray);
+      resolve(dataArray);
+    }).then(function(dataArray){
+      entryToDatabase(dataArray);
+    });
+  });
+}
+
+function entryToDatabase(data){
+  console.log(data);
+  var bawurradb = null;
+  var backenddata = null;
+  var st_data = null;
+    if (device.platform == 'amazon-fireos' || device.platform == 'windows') {
+      bawurradb = window.sqlitePlugin.openDatabase({
+        name: 'bawurradb',
+        location: 'default',
+        androidDatabaseImplementation: 2
+      });
+    }
+    else if (device.platform == 'ios') {
+      bawurradb = window.sqlitePlugin.openDatabase({
+        name: 'bawurradb.sqlite',
+        iosDatabaseLocation: 'Library'
+      });
+    }
+
+      bawurradb.transaction(function (tx){
+        tx.executeSql('DROP TABLE IF EXISTS CULTURE;');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS CULTURE (data);', [], function(res) {
           console.log("DATABASE CREATED");
         }, function(error) {
-          console.log('SELECT SQL statement ERROR: ' + error.message);
+            console.log('SELECT SQL statement ERROR: ' + error.message);
         });
-        tx.executeSql('INSERT INTO CULTURE(data) VALUES (?)', [st_data], function(rs, dat) {
+
+        tx.executeSql('INSERT INTO CULTURE(data) VALUES (?)', [data], function(rs, dat) {
           console.log("INSERTED");
         }, function(error) {
-          console.log('SELECT SQL statement ERROR: ' + error.message);
+            console.log('SELECT SQL statement ERROR: ' + error.message);
         });
+
         tx.executeSql('SELECT * FROM CULTURE;', [], function(tr, dt) {
           backenddata = dt.rows.item(0).data;
           console.log(backenddata);
+          new Promise(function(resolve, reject){
+            for(var i = 0; i < backenddata.length; i++){
+              var a = i;
+              console.log(backenddata[a])
+              JSON.parse(backenddata[a])
+            }
+            resolve();
+          }).then(function(){
+            console.log(backenddata);
+          });
 
         }, function(error) {
           console.log('SELECT SQL statement ERROR: ' + error.message);
         });
-
-      });
-  });
-  onDeviceReady();
-
+    });
 }
+
+function toDataURL(url, callback){
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      callback(reader.result);
+    }
+    reader.readAsDataURL(xhr.response);
+  };
+  xhr.open('GET', url);
+  xhr.responseType = 'blob';
+  xhr.send();
+}
+
+
+
 
   function offline(){
     document.addEventListener("deviceready", onDeviceReady, false);
   }
 
-  function onDeviceReady() {
+
+
+
+function onDeviceReady() {
 
     $.getJSON("data/mainMenu.json", function(result) {
       mainMenu = result;
     }).done(parsePage);
   }
+
+
 
 
 var parsePage = function() {
@@ -127,7 +214,6 @@ var parsePage = function() {
       $("<bold></bold>").text(itemList[item]).css('text-transform', 'capitalize').
       appendTo($(".menuContainer").children().last().children().last().find($("center")));
     }
-
 
     $(document).foundation();
 
